3fe389cd8b471e886879d1843f46cccc735542d8
ptq204 <ptquyen@apcs.vn>
2019-03-28 12:03:46 +0700

    update level 4

--- game.py
+++ game.py
@@ -7,6 +7,7 @@ from search import *
 from AStar import *
 from level1 import *
 from level3 import *
+from level4 import *
 class Game:
 
   def __init__(self):
@@ -23,9 +24,9 @@ class Game:
     self.pacman_img = pygame.image.load('pacman.png')
     self.pacman_img = pygame.transform.scale(self.pacman_img, (square_width, square_height))
     self.monster_img = pygame.image.load('monster.png')
-    self.monster_img = pygame.transform.scale(self.monster_img, (23, 23))
+    self.monster_img = pygame.transform.scale(self.monster_img, (20, 20))
     self.food_img = pygame.image.load('food.png')
-    self.food_img = pygame.transform.scale(self.food_img, (24, 24))
+    self.food_img = pygame.transform.scale(self.food_img, (20, 20))
 
   def drawPacman(self, i, j):
     #pygame.draw.rect(self.screen, red, (x*20, y*20, square_width, square_height), 0)
@@ -48,11 +49,13 @@ class Game:
     self.screen.fill(black)
     filename = ''
     if(level == 1):
-      filename = 'level1_0.txt'
+      filename = 'map10.txt'
     elif(level == 2):
-      filename = 'level2_2.txt'
+      filename = 'map22.txt'
     elif(level == 3):
-      filename = 'map32.txt'
+      filename = 'map30.txt'
+    elif(level == 4):
+      filename = 'map40.txt'
     
     self.map.loadMap(filename)
     self.map.setPos()
@@ -72,9 +75,9 @@ class Game:
     if(code == 1):
       self.drawPacman(d[0], d[1])
     if(code == 2):
+      if(self.map.matrix[s[0]][s[1]] == 2):
+        self.drawFood(s[0], s[1])
       self.drawMonster(d[0], d[1])
-    pygame.display.update()
-    self.fpsClock.tick(12)
   
   def erase(self, i, j):
     pygame.draw.rect(self.screen, black, (j*20, i*20, square_width, square_height))
@@ -96,26 +99,58 @@ class Game:
       for event in pygame.event.get():
         if event.type == QUIT:
           exit()
-
-      if(level == 1 or level == 2):
-        path = BFS(matrix, self.map.pacman, M, N)
+      if(level != 4):
+        if(level == 1 or level == 2):
+          path = BFS(matrix, self.map.pacman, M, N)
+          
+        elif(level == 3):
+          path = hill_climbing(matrix, self.map.pacman, M, N)
         
-      elif(level == 3):
-        path = hill_climbing(matrix, self.map.pacman, M, N)
-      
-      print(path)
-      if(path != None and path != 0):
-        for i in range(len(path)):
-          if(i == 0):
-            s = path[0]
-            d = path[0]
-          else:
-            s = path[i-1]
-            d = path[i]
-          self.moveCharacter(1, s, d)
-          score -= 1
-          if(matrix[d[0]][d[1]] == 2):
-            score += 10
-        #self.erase(path[-1][0], path[-1][1])
-        #self.drawFood(path[-1][0], path[-1][1])
+        print(path)
+        if(path != None and path != 0):
+          for i in range(len(path)):
+            if(i == 0):
+              s = path[0]
+              d = path[0]
+            else:
+              s = path[i-1]
+              d = path[i]
+            self.moveCharacter(1, s, d)
+            score -= 1
+            if(matrix[d[0]][d[1]] == 2):
+              score += 10
+            pygame.display.update()
+            self.fpsClock.tick(9)
+          #self.erase(path[-1][0], path[-1][1])
+          #self.drawFood(path[-1][0], path[-1][1])
+      else:
+        path, ghosts = startGame(matrix, self.map.pacman, M, N)
+        print(path)
+        print(ghosts)
+        gidx = 0
+        if(path != None and path != 0):
+          for i in range(len(path)):
+            if(i == 0):
+              s = path[0]
+              d = path[0]
+            else:
+              s = path[i-1]
+              d = path[i]
+            self.moveCharacter(1, s, d)
+            score -= 1
+            if(matrix[d[0]][d[1]] == 2):
+              score += 10
+            
+            if(len(ghosts[i]) != 0):
+              for j in range(len(ghosts[i])):
+                if(i == 0):
+                  s = ghosts[0][j]
+                  d = ghosts[0][j]
+                else:
+                  s = ghosts[i-1][j]
+                  d = ghosts[i][j]
+                print("Move ghost {} from {} to {}".format(j, s, d))
+                self.moveCharacter(2, s, d)
+            pygame.display.update()
+            self.fpsClock.tick(9)   
       print('score: {}'.format(score))
